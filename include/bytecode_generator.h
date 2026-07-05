d73010a7def759601e2883a5409bb485c92e6fbd
Benjamin Zhong <benjaminzhong1@gmail.com>
2026-07-05 01:19:49 -0400

    Feat: Introduced bytecode generator module's code signatures
    - bytecode generator class
    - filewriter class

---
+++ include/bytecode_generator.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "instruction.h"
+#include <cstdint>
+
+class ByteCodeGeneratorBase
+{
+public:
+    virtual void generateUINT32 (InstructionVector targetInstructionVector, std::string& targetFileName) = 0;
+    virtual void generateINT32  (InstructionVector targetInstructionVector, std::string& targetFileName) = 0;      
+};
---
+++ include/file_handling/file_writer.h
@@ -0,0 +1,8 @@
+#pragma once
+
+template <typename OT, typename IT>
+class FileWriterBase
+{
+public:
+    virtual std::vector<OT> write(std::vector<IT> inputVector) = 0;
+};
