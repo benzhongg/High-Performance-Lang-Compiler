16447e0a4c266b3ac44246c96e89eff41fd87638
Benjamin Zhong <benjaminzhong1@gmail.com>
2026-07-05 01:21:06 -0400

    Feat: High level program flow of the compiler has been written in main
    - first draft of our compilers pipeline using our code signatures

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,45 @@
 #include <iostream>
+#include "preprocessor.h"
+#include "file_handling/file_reader.h"
+#include "macro_resolver.h"
+#include "lexer.h"
+#include "parser.h"
+#include "semantic_analyzer.h"
+#include "instruction_generator.h"
+#include "optimization/optimizer.h"
+#include "bytecode_generator.h"
 
-int main()
+
+int main(int argc, char* argv[])
 {
-	string processedOutput = preprocessor.process(.hplc filepath);
+	//step 1 preprocessing module
+	FileReaderBase*   fileReader   	  { nullptr };
+	PreprocessorBase* preprocessor 	  { nullptr };
+	std::string 	  processedOutput { preprocessor->preprocess(fileReader->readAll()) };
 
-	TokenList tokenList = lexer.tokenize(processedOutput);
+	//step 2 lexer module
+	LexerBase*  lexer 		{ nullptr };
+	TokenVector tokenVector { lexer->tokenize(processedOutput) };
 
-	AST astTree = parser.parse(tokenList);
+	//step 3 parser module
+	ParserBase* parser 	   { nullptr };
+	SyntaxTree  syntaxTree { parser->parse(tokenVector) };
 
-	AST updatedAST = semantic.analyze(astTree);
+	//step 4 semantic analysis module
+	SemanticAnalyzerBase* semanticAnalyzer  { nullptr };
+	SyntaxTree 			  checkedSyntaxTree { semanticAnalyzer->analyze(syntaxTree) };
 
-	InstructionList instrList = IRGenerator.generate(updatedAST);
 
-	InstructionList optimizedIList = optimizer.optimize(instrList);
+	//step 5 instruction generation module
+	InstructionGeneratorBase* instructionGenerator { nullptr };
+	InstructionVector 		  instructionVector    { instructionGenerator->lower(checkedSyntaxTree) };
 
-	ByteCodeFile finalFileOutput = byteCodegenerator.generate(optimizedIList, filename Name);
+	//step 6 optimizer
+	OptimizerBase*    optimizer 				 { nullptr };
+	InstructionVector optimizedInstructionVector { optimizer->optimize(instructionVector) };
+
+	//step 7 byteCodeGenerator
+	ByteCodeGeneratorBase* byteCodeGenerator { nullptr };
+	std::string executableName { };
+	byteCodeGenerator->generateUINT32(optimizedInstructionVector, executableName);
 }
