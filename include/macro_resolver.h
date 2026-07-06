ecfd920cfc46070ed94f0fdd45aabf942b35cf21
Benjamin Zhong <benjaminzhong1@gmail.com>
2026-07-06 16:58:37 -0400

    Feature: added more code signatures  preprocess module
    - HPLC preprocess derived class with main methods
    - missing helper methods
    - DefineMacroResolver class

--- include/macro_resolver.h
+++ include/macro_resolver.h
@@ -1,8 +1,21 @@
 #pragma once
 #include <string>
-
+#include <vector>
 class MacroResolverBase
 {
 public:
-  virtual std::string resolveMacros(std::string source_code) = 0;
+  virtual std::string resolve(std::string source_code) = 0;
 };
+
+using MacroResolverVector = std::vector<MacroResolverBase*>;
+
+class DefineMacroResolver : public MacroResolverBase
+{
+public:
+  std::string resolve(std::string source_code) override
+  {
+  }
+};
+
+
+
